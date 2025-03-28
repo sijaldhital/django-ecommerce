36eea449385ba7a3e55841b72dd5769ee15110ea
sijaldhital <sijaldhital0@gmail.com>
2025-03-28 15:16:29 +0545

    added paypal stuff

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'cart',
     'payment',
     'whitenoise.runserver_nostatic',
+    'paypal.standard.ipn',
 ]
 
 MIDDLEWARE = [
@@ -149,3 +150,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add paypal settings
+# Set sandbox to true
+PAYPAL_TEST = True
+
+PAYPAL_RECEIVER_EMAIL = 'business@sdshop.com' # Business Sandbox account
+
